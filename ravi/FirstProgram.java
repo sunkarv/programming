49babeda170982ccded8c3b034766b349355d2f0
ravindra.sunkaranam <ravindra.sunkaranam@servicenow.com>
2020-10-16 09:29:46 +0530

    added folders for all

---
+++ ravi/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.yb.algorithms.chaitanya;
+
+public class FirstProgram {
+
+	public FirstProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
